493fe3123775cf2908110720528e8a70fdd36d2d
skroof <zulukho0@gmail.com>
2025-07-16 16:36:20 +0200

    add individula print messages to the cases

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -5,16 +5,16 @@ operation = input("Choose the operation (+, -, *, /):")
 message = "The result is"
 match operation:
     case "+":
-        print(message, num1+num2)
+        print("The result is", num1+num2)
     case "-":
-        print(message, num1-num2)
+        print("The result is", num1-num2)
     case "*":
-        print(message, num1*num2)
+        print("The result is", num1*num2)
     case "/":
         if num1 == 0 or num2 == 0:
             print("Cannot devide by zero.")
         else:
-            print(message, num1/num2)
+            print("The result is", num1/num2)
     case _:
         print("Enter a valid number")
 
