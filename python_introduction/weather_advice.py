dfe5cd9de7f2d3489f79d492af5614bedf983b78
skroof <zulukho0@gmail.com>
2025-07-16 15:13:50 +0200

    tets

--- python_introduction/weather_advice.py
+++ python_introduction/weather_advice.py
@@ -1,4 +1,8 @@
+# weather = input("What's the weather like today? (sunny/rainy/cold): ")
+# weather = input("What's the weather like today? (sunny/rainy/cold): ")
 weather = input("What's the weather like today? (sunny/rainy/cold): ")
+
+
 
 if weather == "sunny":
     print("Wear a t-shirt and sunglasses.")
