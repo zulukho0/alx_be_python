21be6cbbbb0b450a47bd314c515c54c2c0a13d4d
skroof <zulukho0@gmail.com>
2025-07-23 17:30:59 +0200

    add new directory and add perform operation to it

---
+++ fns_and_dsa/arithmetic_operations.py
@@ -0,0 +1,15 @@
+def perform_operation(num1: float, num2: float, operation: str):
+    operation = operation.lower()
+    
+    if operation == "add":
+        return num1 +num2
+    elif operation == "subtact":
+        return num1 - num2
+    elif operation == "multiply":
+        return num1 * num2
+    elif operation == "divide":
+        if (num1 == 0) or (num2 == 0):
+            message = "Cannot divide by 0"
+            return message
+        else:
+            return num1 / num2
