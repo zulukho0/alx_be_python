a8ee0e14effc2d7cef0339a5d80035bf3bace49c
skroof <zulukho0@gmail.com>
2025-07-23 17:39:23 +0200

    removed brackets from devide statement

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -8,7 +8,7 @@ def perform_operation(num1, num2, operation):
     elif operation == "multiply":
         return num1 * num2
     elif operation == "divide":
-        if (num1 == 0) or (num2 == 0):
+        if num1 == 0 or num2 == 0:
             message = "Cannot divide by 0"
             return message
         else:
