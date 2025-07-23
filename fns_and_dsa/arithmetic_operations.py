ef0ab89c58dcbc8862b9d07927e636466a4a79b0
skroof <zulukho0@gmail.com>
2025-07-23 17:37:31 +0200

    fix spelling "subtract"

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -3,7 +3,7 @@ def perform_operation(num1, num2, operation):
     
     if operation == "add":
         return num1 +num2
-    elif operation == "subtact":
+    elif operation == "subtract":
         return num1 - num2
     elif operation == "multiply":
         return num1 * num2
