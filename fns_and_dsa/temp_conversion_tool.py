fcb18fcdca5232f574eaf71a3ecfb5d49b2c24ff
skroof <zulukho0@gmail.com>
2025-07-24 13:12:25 +0200

    fixed the conversion type again

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -5,11 +5,11 @@ temp = float(input("Enter the temperature to convert: "))
 conversion = input("Is this temperature in Celsius or Fahrenheit? (C/F):")
 
 def convert_to_celsius(fahrenheit):
-    celsius = FAHRENHEIT_TO_CELSIUS_FACTOR * (fahrenheit-32)
+    celsius = (fahrenheit - 32) * FAHRENHEIT_TO_CELSIUS_FACTOR
     print(f"{fahrenheit}°F is {celsius}°C")
 
 def convert_to_fahrenheit(celsius):
-    fahrenheit = (CELSIUS_TO_FAHRENHEIT_FACTOR * celsius) +32
+    fahrenheit = (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
     print(f"{celsius}°C is {fahrenheit}°F")
 
 
