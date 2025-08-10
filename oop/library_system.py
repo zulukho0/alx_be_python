640948661ddd7951a51389d5c7dc6051b1b564b3
skroof <zulukho0@gmail.com>
2025-08-10 14:44:25 +0200

    add: implement constructor for Book and EBook classes in library_system.py

--- oop/library_system.py
+++ oop/library_system.py
@@ -1,8 +1,14 @@
 class Book:
-    pass
+    def __init__ (self, title: str, auhtor: str):
+        self.title = title
+        self.author = auhtor
+
 
 class EBook(Book):
-    pass
+    def __init__ (self, title: str, auhtor: str, file_size: int, page_count: int):
+        super().__init__(title, auhtor)
+        self.file_size = file_size
+        self.page_count = page_count
 
 class PrintBook(Book):
     pass
