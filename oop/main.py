81e04ada4a48ab172e8a822922801ffb6db8ab38
skroof <zulukho0@gmail.com>
2025-08-10 18:50:40 +0200

    refactor: comment out unused shape-related code in main.py

--- oop/main.py
+++ oop/main.py
@@ -42,17 +42,34 @@
 
 #_______________________End of code for excercise 2 ______________________________
 
-from polymorphism_demo import Shape, Rectangle, Circle
-import math
+# from polymorphism_demo import Shape, Rectangle, Circle
+# import math
+
+# def main():
+#     shapes = [
+#         Rectangle(10, 5),
+#         Circle(7)
+#     ]
+
+#     for shape in shapes:
+#         print(f"The area of the {shape.__class__.__name__} is: {shape.area()}")
+
+# if __name__ == "__main__":
+#     main()
+
+
+#__________________________End of code for exercise 3 _______________________________
+
+from class_static_methods_demo import Calculator
 
 def main():
-    shapes = [
-        Rectangle(10, 5),
-        Circle(7)
-    ]
+    # Using the static method
+    sum_result = Calculator.add(10, 5)
+    print(f"The sum is: {sum_result}")
 
-    for shape in shapes:
-        print(f"The area of the {shape.__class__.__name__} is: {shape.area()}")
+    # Using the class method
+    product_result = Calculator.multiply(10, 5)
+    print(f"The product is: {product_result}")
 
 if __name__ == "__main__":
     main()
